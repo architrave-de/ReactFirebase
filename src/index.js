c60b3126898c441e485b06531a52e987c9f6a081
Ali <ghali@architrave.de>
2019-09-11 10:12:29 +0200

    feat(index): initialize firebase on app start

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import './theme/index.scss'
+import * as serviceWorker from './serviceWorker'
+import {firebaseConfig} from './helpers/configs'
+import {Firebase} from './helpers/firebase'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const initApp = () => {
+        new Firebase(firebaseConfig)
+        return <App/>
+}
+
+ReactDOM.render(initApp(), document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
