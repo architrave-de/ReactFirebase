46ddcd51725176483df999ffb4dcf1e074b14d44
Ali <ghali@architrave.de>
2019-10-22 16:33:52 +0200

    feat(App): create react class, reads app manifest

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
 import React from 'react'
 import HomePage from './containers/HomePage'
+import {DB} from './helpers/firebase'
 
-function App() {
-  return (
-    <HomePage />
-  )
+
+export class App extends React.Component{
+  constructor(props) {
+    super(props)
+    this.state = {
+      appManifest: {
+        appName: '..loading'
+      }
+    }
+  }
+
+  componentDidMount() {
+    DB.collection('manifest').get().then((querySnapshot) => {
+      let dbValues = []
+      querySnapshot.forEach((doc) => {
+        dbValues.push(doc.data())
+      })
+      this.setState({
+        appManifest:{ ...this.state.appManifest,
+          appName: dbValues[0].name
+        }
+      })
+    })
+  }
+
+  render() {
+    return (
+      <HomePage name={this.state.appManifest.appName}/>
+    )
+  }
 }
 
 export default App
