b4d097bd50ff731ea3bd9e4e1050b2d2161214ce
Ali <ghali@architrave.de>
2019-11-14 13:59:23 +0100

    chore(index): apply new code style
    * use more descriptive api methods names

--- src/index.js
+++ src/index.js
@@ -7,16 +7,22 @@ import apiCall from './helpers/API'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 const initApp = () => {
-        const databaseCollections = apiCall.dbCollection()
-        apiCall.getAppManifest(databaseCollections.appManifest).then(data => data[0])
-        .then(data => ReactDOM.render(<App name={data.name}/>, document.getElementById('root')))
+  const databaseCollections = apiCall.dbCollection()
+  apiCall
+    .getCollectionData(databaseCollections.appManifest)
+    .then(data => data[0])
+    .then(data => {
+      let appManifest = data ? data : { name: 'Two Arcs' }
+      ReactDOM.render(
+        <App name={appManifest.name} />,
+        document.getElementById('root')
+      )
+    })
 }
 
 initApp()
 
-
-
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister()
