e5e8bf440187914a64c9dbf3e2dadc3e690c597b
Ali <ghali@architrave.de>
2019-11-24 12:22:33 +0100

    feat(helpers/time): get time information like current day number, month and year

---
+++ src/helpers/timeFunctions.js
@@ -0,0 +1,56 @@
+let oneDay = 1000 * 60 * 60 * 24
+const date = new Date()
+
+/**
+ *
+ * @param {year} [number=2019] {default current year}.
+ * @param {month} [number=12] {default current month} day number to month}.
+ * @param {firstDay} [number=3] {default first day of the given month} day number to month.
+ * @param {lastDay} [number=23] {default last day of the given month} day number to month.
+ */
+
+export function getMonthFirstLastDay({ monthDate = date }) {
+  const thisMonth = monthDate.getMonth()
+  const firstDayOfTheMonth = new Date(monthDate.getFullYear(), thisMonth, 1)
+  const lastDayOfTheMonth = new Date(monthDate.getFullYear(), thisMonth + 1, 0)
+  return {
+    firstDayOfTheMonth,
+    lastDayOfTheMonth
+  }
+}
+
+/**
+ *
+ * @param {year} [number=2019]
+ * @param {startDate} [date=today]
+ */
+export function dayToYear({ startDate = date }) {
+  const yearStartDay = new Date(date.getFullYear(), 0, 0)
+  const diff =
+    startDate -
+    yearStartDay +
+    (date.getTimezoneOffset() - date.getTimezoneOffset()) * 60 * 1000
+  return {
+    year: date.getFullYear(),
+    month: date.getMonth() + 1,
+    dayNumber: Math.floor(diff / oneDay)
+  }
+}
+
+/**
+ *
+ * @param {day} object
+ * @returns day number to year like 19313 to be used to get the matched day rounds
+ */
+export function dayWithYearNumber({ day }) {
+  const yearPrefix = day.year.toString().slice(-2)
+  const dayNumber = day.dayNumber.toString()
+  return parseInt(yearPrefix.concat(dayNumber))
+}
+
+export function startEndDates(startDate, endDate) {
+  return {
+    startDateNumber: dayWithYearNumber({ day: startDate }),
+    endDateNumber: dayWithYearNumber({ day: endDate })
+  }
+}
