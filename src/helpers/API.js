6cc8ce653ce20ba53a77735163b27267339c5cfa
Ali <ghali@architrave.de>
2019-11-24 12:20:54 +0100

    feat(helpers/api): get day/playerse.today scores

--- src/helpers/API.js
+++ src/helpers/API.js
@@ -1,22 +1,41 @@
-import {DB} from './firebase'
-
+import { DB } from './firebase'
+import { dayToYear, dayWithYearNumber } from '../helpers/timeFunctions'
 
 export class API {
-    getAppManifest (collection) {
-        return DB.collection(collection).get().then((querySnapshot) => {
-            let dbValues = []
-            querySnapshot.forEach((doc) => {
-                    dbValues.push(doc.data())
-            })
-            return dbValues
+  getCollectionData(collection, orderBy, orderQuery) {
+    const collectionData = orderBy
+      ? DB.collection(collection).orderBy(orderBy, orderQuery)
+      : DB.collection(collection)
+    return collectionData.get().then(querySnapshot => {
+      let dbValues = []
+      querySnapshot.forEach(doc => {
+        dbValues.push(doc.data())
+      })
+      return dbValues
     })
-    }
+  }
 
-    dbCollection() {
-        return {
-            appManifest: 'manifest',
-        }
+  dbCollection() {
+    return {
+      appManifest: 'manifest',
+      sections: 'sections',
+      players: 'players',
+      days: 'days'
     }
+  }
+
+  get currentDayNumber() {
+    return parseInt(dayWithYearNumber({ day: dayToYear({}) }))
+  }
+
+  get players() {
+    console.log(this.dbCollection.players, this.dbCollection().players)
+    return DB.collection(this.dbCollection().players)
+  }
+
+  get todayScores() {
+    return this.getCollectionData(this.dbCollection().points)
+  }
 }
 
 const apiCall = new API()
