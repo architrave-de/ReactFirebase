3a85e6f9200a713c0d7a3db77580ba210c68d6ad
Ali <ghali@architrave.de>
2019-12-09 15:27:03 +0100

    feat(API/feature): add and featch players

--- src/helpers/API.js
+++ src/helpers/API.js
@@ -1,6 +1,6 @@
 import { DB } from './firebase'
 import { dayToYear, dayWithYearNumber } from '../helpers/timeFunctions'
-
+import { firestore } from 'firebase'
 export class API {
   getCollectionData(collection, orderBy, orderQuery) {
     const collectionData = orderBy
@@ -29,12 +29,117 @@ export class API {
   }
 
   get players() {
-    console.log(this.dbCollection.players, this.dbCollection().players)
     return DB.collection(this.dbCollection().players)
+  }
+
+  get playersList() {
+    return DB.collection(this.dbCollection().players)
+      .orderBy('totalPoints', 'desc')
+      .get()
+      .then(querySnapshot => {
+        let players = []
+        querySnapshot.forEach(function(doc) {
+          players.push({ id: doc.id, ...doc.data() })
+        })
+        return players
+      })
+      .catch(error => {
+        console.log('Error getting the results: ', error)
+      })
+  }
+
+  lastPlayerId() {
+    return this.players.get().then(doc => {
+      let lastId = null
+      doc.forEach(doc => {
+        lastId = doc.id
+      })
+      return lastId
+    })
+  }
+
+  addPlayer({
+    id,
+    name,
+    slackName,
+    image,
+    monthlyRecords = {},
+    email,
+    totalPoints = 0
+  }) {
+    return DB.collection(this.dbCollection().players)
+      .doc(id)
+      .set({ name, slackName, image, monthlyRecords, email, totalPoints })
+      .then(player => {
+        console.log('the player has beed added')
+      })
+      .catch(e => console.log(e))
+  }
+
+  updatePlayer(player) {
+    DB.collection(this.dbCollection().players)
+      .doc(player.id)
+      .set({ ...player })
+      .then(player => {
+        console.log('the player has beed updated---> ', player)
+      })
+      .catch(e => console.log(e))
+  }
+
+  createNewDayEntry({ dayNumber }) {
+    return DB.collection('days')
+      .add({
+        insertDate: new Date(),
+        dayNumber: dayNumber,
+        rounds: []
+      })
+      .then(entryData => {
+        return entryData
+      })
+  }
+
+  addRound({ dayNumber, round }) {
+    const insertRound = doc => {
+      return DB.collection('days')
+        .doc(doc.id)
+        .update({
+          rounds: firestore.FieldValue.arrayUnion({ ...round })
+        })
+    }
+
+    return DB.collection('days')
+      .where('dayNumber', '==', dayNumber)
+      .get()
+      .then(data => {
+        if (!!data.docs[0]) {
+          return insertRound(data.docs[0])
+        } else {
+          return this.createNewDayEntry({ dayNumber: dayNumber }).then(doc => {
+            return insertRound(doc)
+          })
+        }
+      })
   }
 
   get todayScores() {
     return this.getCollectionData(this.dbCollection().points)
+  }
+
+  getDayMatches({ dayNumber }) {
+    return DB.collection('days')
+      .where('dayNumber', '==', dayNumber)
+      .get()
+      .then(querySnapshot => {
+        let matches = []
+        querySnapshot.forEach(function(doc) {
+          matches.push(doc.data())
+        })
+        return matches
+      })
+      .then(data => data)
+      .catch(error => {
+        console.log('Error getting the results: ', error)
+      })
   }
 }
 
