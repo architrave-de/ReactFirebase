4b2f27e44d298ed8731cca7b30b90f188fb82700
Ali <ghali@architrave.de>
2019-12-09 15:16:02 +0100

    feat(containers/homePage): add main app sections

--- src/containers/HomePage/index.js
+++ src/containers/HomePage/index.js
@@ -1,7 +1,53 @@
 import React from 'react'
+import AppNavbar from '../../containers/AppNavbar/AppNavbar'
+import Section from '../../containers/Section/Section'
+import CardsGroup from '../CardsGroup/CardsGroup'
+import TableBoard from '../TableBoard/TableBoard'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import Players from '../Players/Players'
+import Control from '../ControlPanel'
+import MonthlyRecords from '../MonthlyRecords/MonthlyRecords'
 
-const HomePage = ({name}) => (
-    <div><p>{name}</p></div>
-)
+export default class HomePage extends React.Component {
+  render() {
+    return (
+      <div>
+        <Router>
+          <AppNavbar appName={this.props.name} />
+          <Route path="/" exact>
+            <Section title="Todays Records" background="black">
+              <CardsGroup />
+            </Section>
+            <Section title="Monthly Records">
+              <TableBoard />
+            </Section>
+          </Route>
 
-export default HomePage
+          <Route path="/players" exact>
+            <Section title="Players table">
+              <Players />
+            </Section>
+          </Route>
+
+          <Route path="/year" exact>
+            <Section title="Yearly table">
+              <h2>here is the Yearly table</h2>
+            </Section>
+          </Route>
+
+          <Route path="/month" exact>
+            <Section title="Month">
+              <MonthlyRecords />
+            </Section>
+          </Route>
+
+          <Route path="/control-panel" exact>
+            <Section title="Admin Control">
+              <Control />
+            </Section>
+          </Route>
+        </Router>
+      </div>
+    )
+  }
+}
