a99b6ed8dbfb6f88f6d0e92615c6258673877115
Ali <ghali@architrave.de>
2019-12-16 02:40:54 +0100

    feat(containers/userWelcome): display user infos

--- src/containers/ControlPanel/LoginOptions/LoginInInfos.js
+++ src/containers/ControlPanel/LoginOptions/LoginInInfos.js
@@ -1,15 +1,24 @@
 import Alert from 'react-bootstrap/Alert'
 import Button from 'react-bootstrap/Button'
+import Image from 'react-bootstrap/Image'
 import React from 'react'
 
-export default function LoginInfos({ mainButtonAction, title = 'Welcome!' }) {
+export default function LoginInfos({
+  mainButtonAction,
+  title = 'Welcome!',
+  user
+}) {
   return (
     <Alert variant="secondary">
-      <Alert.Heading>{title}</Alert.Heading>
+      <Alert.Heading>
+        {title} {user.name}
+      </Alert.Heading>
       <hr />
+      <Image src={user.image} rounded style={{ width: '96px' }} />
+      <br />
       you are logged in, you can sign out here.
       <div className="d-flex justify-content-end">
-        <Button onClick={() => mainButtonAction({})} variant="outline-success">
+        <Button onClick={() => mainButtonAction({})} variant="outline-info">
           Log me out
         </Button>
       </div>
